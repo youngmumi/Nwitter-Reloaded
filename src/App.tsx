c04884bcd325cd3797189acbd34573a70b93f9bf
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-29 00:31:15 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "Profile",
+        element: <Profile />,
+      }
+    ]
+  }
+])
 
 function App() {
-
-  return <></>;
-
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
