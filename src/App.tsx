66722fc7d2300bbba26e78f4851ae0c5f9bbdd9a
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-30 00:14:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
+import CreateAccount from "./routes/create-account";
+import Login from "./routes/login";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { useState } from "react";
+import { auth } from "./routes/firebase";
 
 const router = createBrowserRouter([
   {
@@ -17,11 +25,51 @@ const router = createBrowserRouter([
         element: <Profile />,
       }
     ]
-  }
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account", 
+    element: <CreateAccount />}
 ])
 
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-FileSystem, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    sans-serif;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
-  return <RouterProvider router={router} />;
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async() => {
+    await auth.authStateReady();
+    setTimeout(() => setIsLoading(false), 2000);
+  }
+  useEffect(() => {
+    init()
+  }, []);
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
+  );
 }
 
 export default App;
