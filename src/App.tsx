6b285d77bf428c6ae74d81cc8080dbb569f9bccc
youngmumi <k0419kyj1@gmail.com>
2024-12-30 20:48:56 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,77 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import CreateAccount from "./routes/create-account";
+import Login from "./routes/login";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { useState } from "react";
+import { auth } from "./routes/firebase";
+import ProtectedRoute from "./components/protected-route";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoute><Layout /></ProtectedRoute>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+
+      },
+      {
+        path: "Profile",
+        element:<Profile />
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account", 
+    element: <CreateAccount />}
+])
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-FileSystem, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    sans-serif;
+  }
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
-
-  return <></>;
-
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async() => {
+    await auth.authStateReady();
+    setTimeout(() => setIsLoading(false), 2000);
+  }
+  useEffect(() => {
+    init()
+  }, []);
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
+  );
 }
 
-export default App
+export default App;
