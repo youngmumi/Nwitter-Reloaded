e076d4261a4c6fca0585b62310730b99c902d44d
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-31 16:51:17 +0900

    Create firebase.ts

---
+++ src/routes/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOryfKyMj5lN93T424uPvZ6Q8S1JMqKrU",
+  authDomain: "nwitter-reloaded-758c7.firebaseapp.com",
+  projectId: "nwitter-reloaded-758c7",
+  storageBucket: "nwitter-reloaded-758c7.firebasestorage.app",
+  messagingSenderId: "99896872125",
+  appId: "1:99896872125:web:ef395345de7d353031bbd4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
