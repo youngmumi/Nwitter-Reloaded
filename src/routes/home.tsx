bcf66482b3699b648afc1df0513d461028ee9f5b
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-31 16:51:34 +0900

    Create home.tsx

---
+++ src/routes/home.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import PosttweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div`
+
+`;
+
+export default function Home(){
+    return (
+        <Wrapper>
+            <PosttweetForm />
+        </Wrapper>
+    );
+}
