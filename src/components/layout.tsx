45b41bc0b330bff371aa2c3abd341c4d2a7437df
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-12-31 16:48:19 +0900

    Create layout.tsx

---
+++ src/components/layout.tsx
@@ -0,0 +1,113 @@
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 1fr 4fr;
+    height: 100%;
+    padding: 50px 0px;
+    width: 100%;
+    max-width: 860px;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none; 
+    color: inherit; 
+    &:focus,
+    &:active {
+        outline: none; 
+    }
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+
+const MenuItem = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    svg {
+        width: 30px;
+        fill: white;
+        stroke: none; /* SVG 테두리 완전히 제거 */
+    }
+    &.log-out {
+        border-color: tomato;
+        svg {
+            fill: tomato;
+        }
+    }
+`;
+
+export default function Layout() {
+    return (
+        <Wrapper>
+            <Menu>
+                <StyledLink to="/">
+                    <MenuItem>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="none"
+                            className="size-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                            />
+                        </svg>
+                    </MenuItem>
+                </StyledLink>
+
+                <StyledLink to="/profile">
+                    <MenuItem>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="none"
+                            className="size-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                            />
+                        </svg>
+                    </MenuItem>
+                </StyledLink>
+
+                <MenuItem className="log-out">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        stroke="none"
+                        className="size-6"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M7.5 3.75A1.5 1.5 0 0 0 6 5.25v13.5a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5V15a.75.75 0 0 1 1.5 0v3.75a3 3 0 0 1-3 3h-6a3 3 0 0 1-3-3V5.25a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3V9A.75.75 0 0 1 15 9V5.25a1.5 1.5 0 0 0-1.5-1.5h-6Zm5.03 4.72a.75.75 0 0 1 0 1.06l-1.72 1.72h10.94a.75.75 0 0 1 0 1.5H10.81l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3a.75.75 0 0 1 1.06 0Z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </MenuItem>
+            </Menu>
+            <Outlet />
+        </Wrapper>
+    );
+}
